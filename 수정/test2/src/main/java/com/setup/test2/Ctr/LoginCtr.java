ac20c58d2d239912061f80cf59c3c9ba52c4e711
DESKTOP-TI1CPI1\User <wlsgmldidhd@naver.com>
2021-01-11 21:00:11 +0900

    LoginCtr update

--- 수정/test2/src/main/java/com/setup/test2/Ctr/LoginCtr.java
+++ 수정/test2/src/main/java/com/setup/test2/Ctr/LoginCtr.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.grp.model.RegisterVO;
+
 import com.setup.test2.Model.EmpVO;
 import com.setup.test2.Model.GradeVO;
 import com.setup.test2.Model.TeamVO;
